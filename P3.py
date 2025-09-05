ca6fb43df70cca477521fdd70f29c92e46a8f093
danna2240666 <9b.hernandez.danna@gmail.com>
2025-09-04 21:11:17 -0500

    Cambios P3

--- P3.py
+++ P3.py
@@ -4,13 +4,17 @@ b= int(input("Ingrese la base b: "))
 if b>9:
     print("Por favor ingrese un valor entre 1 y 9")
 
-d= n/b
-r= n%b
+d= n
 
-L= []
+suma= ""
 
-while d>1:
-    L.append(r)
-    d=d/b
-    r=d%b
-print(L)
+for i in range(1, n+1):
+    if d>1:
+        d= int(d/b**i)
+        r= str(d%b)
+        suma= r + suma
+    elif d==1:
+        suma= "1" + suma
+        print(suma)
+    else:
+        print(suma + "f")
