7ee39a7108d8007e13e09063bdf42106d3e38179
danna2240666 <9b.hernandez.danna@gmail.com>
2025-09-05 21:47:45 -0500

    Cambios taller 1

--- Taller_1/P8.py
+++ Taller_1/P8.py
@@ -7,8 +7,8 @@ b = int(input("limite superior (enteros): "))
 lista = []
 for i in range(n):
     lista.append(rnd.randint(a, b))
-
-elementos = []
+print(lista)
+""""elementos = []
 #for i in range(a, b + 1):
 #    elementos.append(i)
 elementos = [*range(a, b + 1)]
@@ -24,3 +24,23 @@ for j in range(len(elementos)):
         if lista[i] == elementos[j] :
             conteo = conteo + 1
     conteos.append(conteo)
+
+print(elementos)
+print(conteos)
+
+for i in range(len(elementos)):
+    print(f"{elementos[i]} aparece {conteos[i]} veces")
+conteo=0
+
+for i in range(len(lista)):
+    for j in range(len(lista)):
+        if lista[i]==lista[j]:
+            conteo= conteo + 1
+    print(conteo)
+    conteo= 0"""
+menor= lista[0]
+for j in range(lista[1], lista[len(lista)-1]+1):
+    if lista[j]>lista[0]:
+        menor= lista[i]
+a= menor
+print(a)
