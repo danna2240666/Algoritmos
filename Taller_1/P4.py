ffe72869490325c5612a4650a722e66942f5b6ea
danna2240666 <9b.hernandez.danna@gmail.com>
2025-09-08 14:00:45 -0500

    Add files via upload

--- Taller_1/P4.py
+++ Taller_1/P4.py
@@ -5,3 +5,14 @@ suma= ""
 for i in range(len(n)):
     suma= n[i] + suma
 print(suma)
+
+frase= n.split()
+reves= ""
+
+for j in range(len(frase)):
+    orden= ""
+    for k in range(len(frase[j])):
+        orden= (frase[j])[k] + orden
+    reves= reves + orden + " "
+
+print(reves)
